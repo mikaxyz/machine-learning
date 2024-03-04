94c1cab9a19d7ef5e1e71b80b2374801c2877c61
Mika Jauhonen <me@mika.xyz>
2024-03-04 14:33:44 +0100

    Add cli command to train model

---
+++ index.js
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+
+const fs = require("fs");
+const ConcurrentTask = require("@andrewmacmurray/elm-concurrent-task");
+
+
+console.time("total");
+process.on("beforeExit", () => {
+    console.timeEnd("total");
+});
+
+const argv = require("yargs")
+	.usage("Usage: $0 data")
+	.example("$0 /mnist", "TODO")
+	.demandCommand(1, 1, "Need a filename to training data")
+	.argv;
+
+const [fileName] = argv._;
+
+if (!fs.existsSync(fileName)) {
+    return console.error(`File (${fileName}) not found`);
+}
+
+const {Elm} = require("./dist/main.js");
+
+const app = Elm.Main.init({flags: {fileName: fileName}});
+
+ConcurrentTask.register({
+    tasks: {
+        "cli:readFile": readFile,
+        "cli:saveModel": saveModel
+    },
+    ports: {
+        send: app.ports.send,
+        receive: app.ports.receive,
+    },
+});
+
+function readFile({fileName}) {
+    console.log("cli:readFile", fileName)
+    try {
+        const content = fs.readFileSync(fileName, "utf8");
+        let documents = content.split("\n");
+        documents = documents.slice(0, 100);
+
+        console.log("documents.length", documents.length)
+
+        return JSON.stringify(documents.join("\n"));
+    } catch (e) {
+        console.error("cli:readFile", e)
+        return { error: "IOError" };
+    }
+}
+
+function saveModel(model) {
+    console.log("cli:saveModel", model)
+    return { error: "TODO" };
+}
--- src/Main.elm
+++ src/Main.elm
@@ -1,40 +1,269 @@
-module Main exposing (main)
+port module Main exposing (main)
 
-import Browser
-import Browser.Events
-import Html
-import Math.Vector2 as Vec2 exposing (vec2)
-import Math.Vector3 as Vec3
+import ConcurrentTask exposing (ConcurrentTask)
+import Json.Decode as JD
+import Json.Encode as JE
 import Random
 import Task
+import XYZMika.ML.ActivationFunction as ActivationFunction
+import XYZMika.ML.NeuralNetwork as NeuralNetwork exposing (NeuralNetwork)
+
+
+port send : JD.Value -> Cmd msg
+
+
+port receive : (JD.Value -> msg) -> Sub msg
+
+
+type TaskError
+    = DecodeError JD.Error
+    | IOError
+
+
+type TaskSuccess
+    = TrainingData (List ImageData)
 
 
 type Msg
-    = Msg
+    = OnProgress ( ConcurrentTask.Pool Msg TaskError TaskSuccess, Cmd Msg )
+    | OnComplete (ConcurrentTask.Response TaskError TaskSuccess)
+    | Train ImageData
+    | TrainWithData
 
 
-type Model
-    = Model
+type alias Model =
+    { tasks : ConcurrentTask.Pool Msg TaskError TaskSuccess
+    , neuralNetwork : NeuralNetwork
+    , trainingData : List ImageData
+    }
 
 
-main : Program () Model Msg
+type alias Flags =
+    { fileName : String }
+
+
+main : Program Flags Model Msg
 main =
-    Browser.document
-        { init = \_ -> ( Model, Cmd.none )
-        , view =
-            \model ->
-                { title = "Machine Learning"
-                , body = [ Html.text "TODO..." ]
-                }
+    Platform.worker
+        { init = init
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init { fileName } =
+    let
+        ( tasks, cmd ) =
+            ConcurrentTask.attempt
+                { send = send
+                , pool = ConcurrentTask.pool
+                , onComplete = OnComplete
+                }
+                (getFile fileName fileDecoder |> ConcurrentTask.map TrainingData)
+    in
+    ( { tasks = tasks
+      , neuralNetwork =
+            NeuralNetwork.configure
+                { randomSeed = Random.initialSeed 42
+                , inputs = 784
+                , outputs = 10
+                }
+                |> NeuralNetwork.withActivationFunction ActivationFunction.Tanh
+                |> NeuralNetwork.addLayer { neurons = 10 }
+                |> NeuralNetwork.create
+      , trainingData = []
+      }
+    , cmd
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    ConcurrentTask.onProgress
+        { send = send
+        , receive = receive
+        , onProgress = OnProgress
+        }
+        model.tasks
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Msg ->
-            ( model
-            , Cmd.none
+        OnComplete (ConcurrentTask.Success (TrainingData trainingData)) ->
+            let
+                _ =
+                    Debug.log "TrainWithData" (List.length trainingData)
+            in
+            ( { model | trainingData = trainingData }
+            , Task.succeed () |> Task.perform (\_ -> TrainWithData)
             )
+
+        OnComplete error ->
+            let
+                _ =
+                    Debug.log "ERROR" error
+            in
+            ( model, Cmd.none )
+
+        OnProgress ( tasks, cmd ) ->
+            ( { model | tasks = tasks }, cmd )
+
+        TrainWithData ->
+            case model.trainingData of
+                current :: rest ->
+                    ( { model | trainingData = rest }
+                    , Task.succeed current |> Task.perform Train
+                    )
+
+                [] ->
+                    --let
+                    --    _ =
+                    --        Debug.log "TRAIN" imageData.label
+                    --in
+                    ( model
+                    , Cmd.none
+                    )
+
+        Train imageData ->
+            --let
+            --    _ =
+            --        Debug.log "TRAIN" imageData.label
+            --in
+            ( { model
+                | neuralNetwork =
+                    model.neuralNetwork
+                        |> NeuralNetwork.train
+                            { inputs = imageData.values |> List.map (\x -> toFloat x / 255)
+                            , expected = labelToOutput10 imageData.label
+                            }
+              }
+            , Task.succeed () |> Task.perform (\_ -> TrainWithData)
+            )
+
+
+labelToOutput10 : Int -> List number
+labelToOutput10 label =
+    case label of
+        0 ->
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ]
+
+        1 ->
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 ]
+
+        2 ->
+            [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 ]
+
+        3 ->
+            [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 ]
+
+        4 ->
+            [ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ]
+
+        5 ->
+            [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 ]
+
+        6 ->
+            [ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 ]
+
+        7 ->
+            [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 ]
+
+        8 ->
+            [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+        _ ->
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+
+type alias ImageData =
+    { label : Int
+    , values : List Int
+    }
+
+
+imageDataFromString : String -> Result Int ImageData
+imageDataFromString x =
+    case String.split "," x |> List.map String.toInt of
+        (Just label) :: data ->
+            { label = label
+            , values = data |> List.filterMap identity
+            }
+                |> Ok
+
+        _ ->
+            Err 0
+
+
+imageDataDecoder : JD.Decoder ImageData
+imageDataDecoder =
+    JD.string
+        |> JD.andThen
+            (\x ->
+                case imageDataFromString x of
+                    Ok imageData ->
+                        JD.succeed imageData
+
+                    Err _ ->
+                        JD.fail "Could not decode image data"
+            )
+
+
+fileDecoder : JD.Decoder (List ImageData)
+fileDecoder =
+    JD.string
+        |> JD.andThen
+            (\fileContent ->
+                let
+                    results : { errors : List Int, images : List ImageData }
+                    results =
+                        String.lines fileContent
+                            |> List.map imageDataFromString
+                            |> List.foldl
+                                (\x a ->
+                                    case x of
+                                        Err err ->
+                                            { a | errors = err :: a.errors }
+
+                                        Ok image ->
+                                            { a | images = image :: a.images }
+                                )
+                                { errors = [], images = [] }
+                in
+                case ( results.errors, results.images ) of
+                    ( [], [] ) ->
+                        JD.fail "No data"
+
+                    ( errors, [] ) ->
+                        JD.fail ("Failed to decode " ++ (List.length errors |> String.fromInt) ++ " rows")
+
+                    ( _, images ) ->
+                        JD.succeed images
+            )
+
+
+decodeErrors : JD.Decoder TaskError
+decodeErrors =
+    JD.string
+        |> JD.andThen
+            (\reason ->
+                case reason of
+                    "IOError" ->
+                        JD.succeed IOError
+
+                    _ ->
+                        JD.fail ("Unrecognized ReadError " ++ reason)
+            )
+
+
+getFile : String -> JD.Decoder (List ImageData) -> ConcurrentTask TaskError (List ImageData)
+getFile fileName decoder =
+    ConcurrentTask.define
+        { function = "cli:readFile"
+        , expect = ConcurrentTask.expectString
+        , errors = ConcurrentTask.expectErrors decodeErrors
+        , args = JE.object [ ( "fileName", JE.string fileName ) ]
+        }
+        |> ConcurrentTask.map (JD.decodeString decoder >> Result.mapError DecodeError)
+        |> ConcurrentTask.andThen ConcurrentTask.fromResult
